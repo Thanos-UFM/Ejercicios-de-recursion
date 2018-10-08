b8c0c675182dcae00246b53cbbcb0985b8e07e89
alejclemus <38306162+alejclemus@users.noreply.github.com>
2018-10-08 09:47:00 -0600

    Factorial

--- Main.java
+++ Main.java
@@ -2,18 +2,17 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-
-    private static int suma (int n){
+    private static int factorial(int n){
         if (n==0)
-            return 0;
+            return 1;
         else
-            return suma(n-1)+n;
+            return factorial(n-1)*n;
     }
 
     public static void main(String[] args) {
         int n;
         Scanner scan= new Scanner(System.in);
         n=scan.nextInt();
-        System.out.print(suma(n));
+        System.out.print(factorial(n));
     }
 }
